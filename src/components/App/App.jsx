b948b45cb2a14cb87f05fc82324c9c6f5852cd5e
ALEXANDR ZHDANKO <zhdko@yandex.ru>
2023-09-03 21:40:13 +0400

    Удалить импорт шапки страницы из App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from '../Header/Header';
 // #TODO Header
 
 function App() {
-  return (
-    <div className="page">
-      <Header />
-    </div>
-  );
+  return <div className="page" />;
 }
 
 export default App;
