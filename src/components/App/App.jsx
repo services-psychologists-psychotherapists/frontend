8f7826c1fee29265a78014d0f1d112f330e3d96f
TouristOnAMission <maikl.mikhael@yandex.ru>
2023-09-05 23:30:53 +0300

    fix: почистить App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,7 @@
-// import React from 'react';
-// import './App.css';
-// // #TODO Header
-
-// export default function App() {
-//   return <div className="page" />;
-// }
-
 import React from 'react';
 import './App.css';
-import Header from '../Header/Header';
-import Banner from '../Banner/Banner';
-import EmergencyHelp from '../EmergencyHelp/EmergencyHelp';
-import TermsOfCooperation from '../TermsOfCooperation/TermsOfCooperation';
-import Footer from '../Footer/Footer';
+// #TODO Header
 
 export default function App() {
-  return (
-    <div className="page">
-      <Header />
-      <Banner />
-      <EmergencyHelp />
-      <TermsOfCooperation />
-      <Footer />
-    </div>
-  );
+  return <div className="page" />;
 }
