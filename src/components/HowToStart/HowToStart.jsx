ca63d71574979ba01d8fac9ddfa070ff2209b158
Three Worlds <harrymidas@yandex.ru>
2023-09-09 20:44:22 +0300

    добавляет блок how-to-start

--- src/components/HowToStart/HowToStart.jsx
+++ src/components/HowToStart/HowToStart.jsx
@@ -11,15 +11,30 @@ export default function HowToStart() {
       <ul className="how-to-start__cards">
         {LIST_OF_STEPS.map((el) => {
           if (el.content === 'image') {
-            <li className="how-to-start__card-img" key={el.numbKey}>
-              <img
-                src={el.imgPath}
-                className="how-to-start__img"
-                alt={el.imgPath}
-              />
-            </li>;
+            return (
+              <li className="how-to-start__card-img" key={el.numbKey}>
+                <img
+                  src={el.imgPath}
+                  className="how-to-start__img"
+                  alt={el.imgPath}
+                />
+              </li>
+            );
           }
-          return console.log('teeeeeeeext');
+          if (el.content === 'text') {
+            return (
+              <li className="how-to-start__card" key={el.numbKey}>
+                <span className="how-to-start__subtitle-span">
+                  {el.numberStep}
+                </span>
+                <p className="how-to-start__subtitle">{el.nameStep}</p>
+                <p className="how-to-start__description">
+                  {el.descriptionStep}
+                </p>
+              </li>
+            );
+          }
+          return null;
         })}
       </ul>
     </section>
