e8154f2e4603efe0da326679f2c4372ae2f0647d
Aleksandr Komolkin <san4es.kom@gmail.com>
2023-09-14 13:53:40 +0300

    feat: Создан компонент Input и хук для него со встроенной валидацией

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,128 @@
+import './Input.css';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useForm } from '../../hooks/useForm';
+import closedEye from '../../images/Input/input-container__closed-eye.svg';
+import openedEye from '../../images/Input/input-container__opened-eye.svg';
+import openedEyeError from '../../images/Input/input-container__opened-eye_error.svg';
+import closedEyeError from '../../images/Input/input-container__closed-eye_error.svg';
+
+export default function Input({
+  title,
+  type,
+  name,
+  prompt,
+  disabled,
+  placeholder,
+  minLength,
+  maxLength,
+}) {
+  const [isEyeOpened, setIsEyeOpened] = useState(false);
+
+  const { values, handleChange, errors, isValid } = useForm({
+    [name]: '',
+  });
+
+  function showPasswordContent(e) {
+    e.preventDefault();
+    setIsEyeOpened(!isEyeOpened);
+  }
+
+  return (
+    <div className="input-container">
+      <label
+        htmlFor={name}
+        className={`input-container__label ${
+          disabled && 'input-container__label_disabled'
+        }`}
+      >
+        {title}
+      </label>
+      <div className="input-container__input-wrapper">
+        {type === 'textarea' ? (
+          <textarea
+            id={name}
+            name={name}
+            value={values[name] || ''}
+            onChange={handleChange}
+            required
+            className={`input-container__input input-container__textarea-input ${
+              !isValid && !disabled && 'input-container__invalid-input'
+            } ${disabled && 'input-container__input_disabled'}`}
+            placeholder={placeholder}
+            disabled={disabled}
+            minLength={minLength}
+            maxLength={maxLength}
+          />
+        ) : (
+          <input
+            type={isEyeOpened ? 'text' : type}
+            id={name}
+            name={name}
+            value={values[name] || ''}
+            onChange={handleChange}
+            required
+            className={`input-container__input ${
+              !isValid && !disabled && 'input-container__invalid-input'
+            } ${disabled && 'input-container__input_disabled'}`}
+            placeholder={placeholder}
+            disabled={disabled}
+            minLength={minLength}
+            maxLength={maxLength}
+          />
+        )}
+        {type === 'password' && (
+          <button
+            className="input-container__input-icon"
+            onClick={showPasswordContent}
+          >
+            <img
+              alt="Eye-icon"
+              src={
+                // eslint-disable-next-line no-nested-ternary
+                isEyeOpened
+                  ? isValid
+                    ? openedEye
+                    : openedEyeError
+                  : isValid
+                  ? closedEye
+                  : closedEyeError
+              }
+            />
+          </button>
+        )}
+      </div>
+      {(errors[name] && (
+        <span className="input-container__span_visible input-container__span_visible-error">
+          {errors[name]}
+        </span>
+      )) ||
+        (prompt && !errors.name && !disabled && (
+          <span className="input-container__span_visible input-container__span_visible-prompt">
+            {prompt}
+          </span>
+        )) || (
+          <span className="input-container__span_not-visible">
+            {errors[name]}
+          </span>
+        )}
+    </div>
+  );
+}
+
+Input.propTypes = {
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  prompt: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
+  placeholder: PropTypes.string.isRequired,
+  minLength: PropTypes.string,
+  maxLength: PropTypes.string,
+};
+
+Input.defaultProps = {
+  disabled: false,
+  minLength: '',
+  maxLength: '',
+};
--- src/components/PsychologistProfile/PsychologistProfile.jsx
+++ src/components/PsychologistProfile/PsychologistProfile.jsx
@@ -1,12 +1,41 @@
 import './PsychologistProfile.css';
 import React from 'react';
 import Header from '../Header/Header';
+import Input from '../Input/Input';
 
 export default function PsychologistProfile() {
   return (
     <>
       <Header isLoggedIn={false} />
-      <div className="psychologist-page" />
+      <div className="psychologist-page">
+        <form name="login" noValidate>
+          <Input
+            name="login"
+            title="Логин"
+            type="email"
+            prompt="Здесь должна быть подсказка"
+            placeholder="irina_k@yan.ru"
+          />
+          <Input
+            name="password"
+            title="Пароль"
+            type="password"
+            prompt="Здесь должна быть подсказка"
+            placeholder="********"
+            maxLength="32"
+            minLength="8"
+          />
+          <Input
+            name="textarea"
+            title="Комментарий"
+            type="textarea"
+            prompt="Здесь должна быть подсказка"
+            placeholder="Комментарий"
+            maxLength="32"
+            minLength="8"
+          />
+        </form>
+      </div>
     </>
   );
 }
