7f793d6fcbdc92311dd631f09cc0efd811e87401
ALEXANDR ZHDANKO <zhdko@yandex.ru>
2023-09-15 22:45:57 +0400

    feat: добавить компонен  имени психолога

---
+++ src/utils/getAge.js
@@ -0,0 +1,9 @@
+export default function getAge(date) {
+  const today = new Date();
+  let age = today.getFullYear() - date.getFullYear();
+  const month = today.getMonth() - date.getMonth();
+  if (month < 0 || (month === 0 && today.getDate() < date.getDate())) {
+    age -= 1;
+  }
+  return age;
+}
