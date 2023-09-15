8c53700e7831ea3b280f2f795473474fed7ca24f
ALEXANDR ZHDANKO <zhdko@yandex.ru>
2023-09-15 15:57:48 +0400

    feat: добавить компонент аватара

---
+++ src/components/generic/Avatar/Avatar.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Avatar.css';
+import noAvatar from '../../../images/no-avatar.svg';
+
+export default function Avatar({ src, size }) {
+  function getNoAvatarClasses(tag) {
+    if (tag === 'img') {
+      if (size === 'xs' || size === 's') {
+        return 'img__size_s';
+      }
+      return '';
+    }
+    if (src === noAvatar) {
+      return `${tag}__no-avatar`;
+    }
+    return '';
+  }
+
+  return (
+    <div
+      className={`avatar avatar_size_${size} ${getNoAvatarClasses('avatar')}`}
+    >
+      <img
+        src={src}
+        alt="аватар пользователя"
+        className={`img ${getNoAvatarClasses('img')}`}
+      />
+      {size === 'xl' && (
+        <button
+          type="button"
+          className={`camera-icon ${getNoAvatarClasses('camera-icon')}`}
+        />
+      )}
+    </div>
+  );
+}
+
+const { string } = PropTypes;
+
+Avatar.propTypes = {
+  src: string,
+  size: string,
+};
+
+Avatar.defaultProps = {
+  size: 'xl',
+  src: noAvatar,
+};
