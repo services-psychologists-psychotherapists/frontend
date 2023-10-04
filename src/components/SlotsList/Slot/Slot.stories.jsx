6f805f4532335f692b2ccaf521af30ad9656aca4
TouristOnAMission <maikl.mikhael@yandex.ru>
2023-10-04 16:56:49 +0300

    fix: исправить ошибки

--- src/components/SlotsList/Slot/Slot.stories.jsx
+++ src/components/SlotsList/Slot/Slot.stories.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'storybook-addon-react-router-v6';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { useArgs } from '@storybook/client-api';
 import Slot from './Slot';
 
