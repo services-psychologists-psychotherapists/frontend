a78b03e5d9769e80097662dc124a7ba48937cb17
Mikhail Sedlachek <112722262+sdlmdev@users.noreply.github.com>
2023-10-10 01:43:43 +0300

    Update Prompt.jsx

--- src/components/Fieldset/Prompt/Prompt.jsx
+++ src/components/Fieldset/Prompt/Prompt.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Prompt({
-  prompt, disabled, errors, values
+  prompt, disabled, errors, values, promptClasses,
 }) {
   const errorClassName = errors ? 'field-container__prompt_visible-error' : '';
   const promptClassName = prompt && !errors && !disabled && !values ? 'field-container__prompt_visible-prompt' : '';
@@ -12,7 +12,9 @@ export default function Prompt({
   const trimmedErrors = errors && errors.length > maxErrorLength ? `${errors.substring(0, maxErrorLength)}...` : errors;
 
   return (
-    <span className={`field-container__prompt ${errorClassName} ${promptClassName}`}>
+    <span className={`field-container__prompt ${
+      promptClasses || 'field-container__prompt-height'} ${errorClassName} ${promptClassName}`}
+    >
       {trimmedErrors || prompt}
     </span>
   );
@@ -23,6 +25,7 @@ Prompt.propTypes = {
   disabled: PropTypes.bool,
   errors: PropTypes.string,
   prompt: PropTypes.string,
+  promptClasses: PropTypes.string,
 };
 
 Prompt.defaultProps = {
@@ -30,4 +33,5 @@ Prompt.defaultProps = {
   values: null,
   prompt: null,
   errors: null,
+  promptClasses: '',
 };
